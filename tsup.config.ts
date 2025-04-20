22257ae1d79327d8f038be51a0089ccb77a2510d
Lee Jongwoo <jongwoo328@gmail.com>
2025-04-20 23:28:59 +0900

    update build configs

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,16 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["src/index.ts"],
 	format: ["esm", "cjs"],
+	outDir: "dist",
 	dts: {
 		entry: "src/index.ts",
 	},
 	sourcemap: true,
 	clean: true,
 	target: "es2022",
+	outExtension({ format }) {
+		return {
+			js: format === "esm" ? ".mjs" : ".js",
+		};
+	},
 });
